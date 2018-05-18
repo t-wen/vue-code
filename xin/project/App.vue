e36c5b98d49ac5fa67913afb9ebbc1593283858f
t-wen <30753500+t-wen@users.noreply.github.com>
2018-05-18 18:02:13 +0800

    Initial commit

---
+++ xin/project/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="app">
+    <h1 v-text="title"></h1>
+  </div>
+</template>
+
+<script>
+export default {
+  data: function () {
+    return {
+      title: 'this is a todo list'
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
